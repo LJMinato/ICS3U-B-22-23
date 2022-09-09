a3fb53e38d847f05937e81d13201995b34fb858c
Lucca Minato <lminato@bayviewglen.ca>
2022-09-09 11:14:26 -0400

    Initial commit

---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleOne.java
@@ -0,0 +1,24 @@
+package day2;
+
+/*
+ * Comment
+ */
+
+/**
+ * ExampleOne
+ */
+public class ExampleOne {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to computer science");
+        System.out.println("Welcome to computer science");
+        System.out.println("Welcome to computer science");
+
+        System.out.print("A");
+        System.out.print("B");
+        System.out.print("C");
+        System.out.println("D");
+        System.out.print("E");
+        System.out.println("F");
+    }
+}
---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -0,0 +1,20 @@
+package day2;
+
+/*
+ * This is abput escape sequences
+ */
+
+/**
+ * ExampleTwo
+ */
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("Computer Scie\nce is the \"BEST\" class ever");
+        /*
+         * escape sequense is meant to give another meaning to a character
+         * I should know /""
+         * I should know /n
+         * I should know //
+         */
+    }
+}
